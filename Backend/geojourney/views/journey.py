ff785598b2fadee85fdc10d6f2700f2bb8d21216
indionapolis <panddromas@gmail.com>
2019-10-26 22:45:36 +0300

    addede queryset

--- Backend/geojourney/views/journey.py
+++ Backend/geojourney/views/journey.py
@@ -5,12 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from django.conf import settings
-
+from geojourney.models import Journey
 from geojourney.serializers.journey import CreateJourneySerializer, JourneySerializer
 
 
 class JourneyViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, GenericViewSet):
     serializer_class = JourneySerializer  # WIP
+
+    queryset = Journey.objects.all()
 
     # permission_classes = (IsAuthenticated,)
 
